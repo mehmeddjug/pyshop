b3cb0f93ff7b7815bfa36d2a83d6f9583b7f77e8
mehmeddjug <mehmed.djug@outlook.com>
2024-12-15 14:41:47 +0100

    Initial commit for product and order apps.

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import config
+from . import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
+    'product',
+    'order',
 ]
 
 MIDDLEWARE = [
